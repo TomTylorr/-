9e12368a86de5643037dfc5a0f3e7eccd60863a4
Dima <Dima@DIMANOTEBOOK>
2026-02-13 20:52:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Hello;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+try
+{
+    short a = 20000, b = 20000;
+    // Вызываем Add напрямую, если он в этом же файле, или через Program.Add
+    short c = checked((short)MyProgram.Add(a, b)); 
+    Console.WriteLine($"c = {c}");
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine("Произошло переполнение: " + ex.Message);
+}
---
+++ Моя.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Hello;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        short a = 20000, b = 20000;
+        int c = Add(a, b);
+        Console.WriteLine("с = {0}", c);
+        Console.ReadLine();
+    }
+
+    public static int Add(int a, int b)
+    {
+        return a + b; 
+    }
+}
+
+
+
